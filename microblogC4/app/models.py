201932f9a6723fc014a7fc0d14690d2a18b113cc
Astrid Wang <astrid@Astrids-MBP.lan1>
2020-05-02 19:06:25 -0400

    em,

--- microblogC4/app/models.py
+++ microblogC4/app/models.py
@@ -6,6 +6,9 @@ class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Classroom = db.Column(db.String(32), index=True, unique=False)
     Course_code = db.Column(db.String(32), index=True, unique=False)
+    session_ID = db.Column(db.Integer, index = True, unique = True)# then migrate, add a new col in spreadsheet, check the existing code read the xl function, check update(match code+seesionID
+
+
     Course_title = db.Column(db.String(128))
     Credits = db.Column(db.Integer)
     Instructor = db.Column(db.String(128))
@@ -16,4 +19,12 @@ class Course(db.Model):
     def __repr__(self):
         return '<Course {}>'.format(self.Course_title)
 
+class Student(db.Model):
+    SID = db.Column(db.Integer, primary_key=True)
+    Sname = db.Column(db.String(128))
+    Courses = 
+
+class Instructor(db.Model):
+    TID = db.Column(db.Integer, primary_key=True)
+    Tname = db.Column(db.String(128))
 
