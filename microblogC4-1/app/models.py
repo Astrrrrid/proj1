ce5484461c7240b5123e628e88d2dd8c825c1754
Astrid Wang <astrid@Astrids-MBP.lan1>
2020-06-17 15:47:50 -0400

    changed tables to class models

--- microblogC4-1/app/models.py
+++ microblogC4-1/app/models.py
@@ -14,17 +14,32 @@ colNames = {"Student":["SID", "Sname", "Semail", "Address", "Dpt", "GradDate"], 
     "Instructs":["TID", "session_ID", "PrimaryTeach"]} 
 
 
-Enrolls = db.Table('Enrolls',
+'''Enrolls = db.Table('Enrolls',
     db.Column('SID', db.Integer, db.ForeignKey('student.SID'), primary_key=True),
     db.Column('session_ID', db.String(32), db.ForeignKey('course.session_ID'), primary_key=True),
     db.Column('Status', db.String(32)) # -- student is on waitlist with a number, or auditing, or registrated with credits
-)
+)'''
 
-Instructs = db.Table('Instructs',
+'''Instructs = db.Table('Instructs',
     db.Column('TID', db.Integer, db.ForeignKey('instructor.TID'), primary_key=True),
     db.Column('session_ID', db.String(32), db.ForeignKey('course.session_ID'), primary_key=True),
     db.Column('PrimaryTeach', db.Boolean, nullable=True, default=False) # -- the host who is in charge of the course and leads all speakers 
-)
+)'''
+
+class Instructs(db.Model):
+    TID = db.Column(db.Integer, db.ForeignKey('instructor.TID'), primary_key=True)
+    session_ID = db.Column(db.String(32), db.ForeignKey('course.session_ID'), primary_key=True)
+    PrimaryTeach = db.Column(db.Boolean, nullable=True, default=False) # -- the host who is in charge of the course and leads all speakers
+    course = relationship(Course, backref=backref("Instructs", cascade="all, delete-orphan"))
+    instructor = relationship(Instructor, backref=backref("Instructs", cascade="all, delete-orphan"))
+
+class Enrolls(db.Model):
+    SID = db.Column(db.Integer, db.ForeignKey('student.SID'), primary_key=True)
+    session_ID = db.Column(db.String(32), db.ForeignKey('course.session_ID'), primary_key=True)
+    Status = db.Column(db.String(32)) # -- student is on waitlist with a number, or auditing, or registrated with credits
+    course = relationship(Course, backref=backref("Enrolls", cascade="all, delete-orphan"))
+    student = relationship(Student, backref=backref("Enrolls", cascade="all, delete-orphan"))
+
 
 class Course(db.Model):
     #id = db.Column(db.Integer)
